a7d26f776d51d12ede5b0b1aa66703c5c9bfd233
mldamico <matias.lioneldamico@gmail.com>
2023-09-10 20:21:07 -0300

    using automapper inside users controller

--- Petty/Controllers/UsersController.cs
+++ Petty/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Petty.Data;
+using Petty.DTO;
 using Petty.Entities;
 using Petty.Interfaces;
 
@@ -9,28 +11,33 @@ namespace Petty.Controllers;
 public class UsersController : BaseController
 {
     private readonly IUserRepository _userRepository;
-    public UsersController(IUserRepository userRepository)
+    private readonly IMapper _mapper;
+
+    public UsersController(IUserRepository userRepository, IMapper mapper)
     {
         _userRepository = userRepository;
+        _mapper = mapper;
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
-        return Ok(await _userRepository.GetUsersAsync());
+        var users = await _userRepository.GetUsersAsync();
+        var usersReturning = _mapper.Map<IEnumerable<MemberDto>>(users);
+        return Ok(usersReturning);
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<AppUser>> GetUserById(int id)
+    public async Task<ActionResult<MemberDto>> GetUserById(int id)
     {
         var user = await _userRepository.GetUserByIdAsync(id);
-        return user;
+        return _mapper.Map<MemberDto>(user);
     }
 
     [HttpGet("{username}")]
-    public async Task<ActionResult<AppUser>> GetUserByUsername(string username)
+    public async Task<ActionResult<MemberDto>> GetUserByUsername(string username)
     {
         var user = await _userRepository.GetUserByUsernameAsync(username);
-        return user;
+        return _mapper.Map<MemberDto>(user);
     }
 }
